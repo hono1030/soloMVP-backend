362aa08bd139dd0453cb76d7420ed631d7e34188
hono1030 <honoka.n1030@gmail.com>
2024-11-07 15:13:05 +0900

    chore: change db env name

--- knexfile.js
+++ knexfile.js
@@ -4,12 +4,12 @@ const DB_USER = process.env.DB_USER;
 const DB_NAME = process.env.DB_NAME;
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
-const DB_URL = process.env.DB_URL;
+const DATABASE_URL = process.env.DATABASE_URL;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
 module.exports = {
   client: "postgresql",
-  connection: DB_URL || {
+  connection: DATABASE_URL || {
     host: DB_HOST,
     port: DB_PORT,
     database: DB_NAME,
