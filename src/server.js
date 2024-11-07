b4cebd530729fcff78a99beb6d77d3e09acf30ee
hono1030 <honoka.n1030@gmail.com>
2024-11-07 17:35:19 +0900

    fix error

--- src/server.js
+++ src/server.js
@@ -32,12 +32,12 @@ const sessionSecretKey = crypto.randomBytes(32).toString("hex");
 // middlewareConfiguration
 app.set("trust proxy", 1);
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://your-netlify-app.netlify.app'/);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'https://your-netlify-app.netlify.app'/);
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+//   });
 
 // middleware
 app.use(
